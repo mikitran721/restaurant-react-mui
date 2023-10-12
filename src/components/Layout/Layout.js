7686d1225fa60b3f378393051e827743c5169898
MikiTran <mikitran721@gmail.com>
2023-10-12 23:07:00 +0700

    initial project with route&MUI

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <h2>Layout</h2>
+    </>
+  );
+};
+
+export default Layout;
